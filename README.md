0f7129f9d4ccee7af5789c6b5c0d7651cc83f4e5
Rodolfo Paranhos <ropbla9@gmail.com>
2018-06-25 20:13:08 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # vue-smart-image
+
+A image that fits everywhere it does not matter the size. If it find not source it will render a sample image.
+Just give this component a 'src' prop and let it magically render the image.
---
+++ index.vue
@@ -0,0 +1,41 @@
+<style scoped>
+
+    .smart-image .inner {
+        margin: auto;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: contain;
+    }
+
+</style>
+
+<template>
+    <div class="smart-image" :style="{ padding }">
+
+        <div class="inner" :style="{ backgroundImage: 'url(' + _src + ')', width, height }" />
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+
+        name: 'smart-image',
+
+        computed: {
+            _src() {
+                return this.src === null ? require('./no-image.jpg') : this.src
+            }
+        },
+
+        props: {
+            src: String,
+            width: String,
+            height: String,
+            padding: String
+        }
+
+    }
+
+</script>
